d50bf1934481b6e7fe14bb332d6e09235bc92671
Nicholas-Homann <166616343+Nicholas-Homann@users.noreply.github.com>
2025-02-10 16:45:48 +0000

    Update InboundRoad.py: Uncomment Initialization

--- build/InboundRoad.py
+++ build/InboundRoad.py
@@ -2,9 +2,9 @@ class InboundRoad:
     def __init__(self):
         self.total_standard_lanes = 1
         # Commented out till implementation
-        # self.has_bus_lane = False
-        # self.has_left_lane = False
-        # self.priority_factor = 0
+        self.has_bus_lane = False
+        self.has_left_lane = False
+        self.priority_factor = 0
 
     # Here we return the number of lanes that the road has 
     def total_standard_lanes(self):
