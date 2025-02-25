1fa8b304682b968a024509127d3261e8b6490294
Lucy Siket <lucysiket@outlook.com>
2025-02-25 18:36:29 +0000

    Improving with arrays
    Made code less complex by using an array instead of if statements

--- build/app.py
+++ build/app.py
@@ -8,29 +8,19 @@ app = Flask(__name__,
 
 # Initialises the true model objects
 junction_model = Junction.Junction()
-
-# North, east, south and west junction arms
-north_lane = InboundRoad.InboundRoad()
-east_lane = InboundRoad.InboundRoad()
-west_lane = InboundRoad.InboundRoad()
-south_lane = InboundRoad.InboundRoad()
+lanes = [InboundRoad.InboundRoad()] * 4
 
 selected_lane = None
+
 
 # Reset temp model
 # This creates the temporary object for editing
 temp_junction_model = Junction.Junction()
-
-# North, east, south and west junction arms
-temp_north_lane = InboundRoad.InboundRoad()
-temp_east_lane = InboundRoad.InboundRoad()
-temp_west_lane = InboundRoad.InboundRoad()
-temp_south_lane = InboundRoad.InboundRoad()
-
-
-# Make an array of inbound roads
-# Set selected lane to an index
-
+temp_lanes = [InboundRoad.InboundRoad()] * 4
+# 0 = north
+# 1 = east
+# 2 = south
+# 3 = west
 
 # Serve the main HTML page
 @app.route("/")
@@ -44,44 +34,35 @@ def home():
 def reset_temp_model():
     # This creates the temporary object for editing
     global temp_junction_model
-    global temp_north_lane
-    global temp_south_lane
-    global temp_east_lane
-    global temp_west_lane
-
+    global temp_lanes
     temp_junction_model = Junction.Junction()
-
-    # North, east, south and west junction arms
-    temp_north_lane = InboundRoad.InboundRoad()
-    temp_east_lane = InboundRoad.InboundRoad()
-    temp_west_lane = InboundRoad.InboundRoad()
-    temp_south_lane = InboundRoad.InboundRoad()
+    temp_lanes = [InboundRoad.InboundRoad()] * 4
 
 @app.route("/edit-northbound")
 def edit_northbound():
     global selected_lane
-    selected_lane = "north"
+    selected_lane = 0
     reset_temp_model()
     return Response(status=204)
 
 @app.route("/edit-eastbound")
 def edit_eastbound():
     global selected_lane
-    selected_lane = "east"
+    selected_lane = 1
     reset_temp_model()
     return Response(status=204)
 
 @app.route("/edit-southbound")
 def edit_southbound():
     global selected_lane
-    selected_lane = "south"
+    selected_lane = 2
     reset_temp_model()
     return Response(status=204)
 
 @app.route("/edit-westbound")
 def edit_westbound():
     global selected_lane
-    selected_lane = "west"
+    selected_lane = 3
     reset_temp_model()
     return Response(status=204)
 
@@ -90,77 +71,30 @@ def edit_westbound():
 # Add lane
 @app.route("/add-lane")
 def add_lane():
-    if selected_lane == "north":
-        temp_north_lane.increment_num_lanes()
-        print("Num lanes:" + str(temp_north_lane.get_num_lanes()))
-    elif selected_lane == "east":
-        temp_east_lane.increment_num_lanes()
-        print("Num lanes:" + str(temp_east_lane.get_num_lanes()))
-    elif selected_lane == "south":
-        temp_south_lane.increment_num_lanes()
-        print("Num lanes:" + str(temp_south_lane.get_num_lanes()))
-    elif selected_lane == "west":
-        temp_west_lane.increment_num_lanes()
-        print("Num lanes:" + str(temp_west_lane.get_num_lanes()))
-    else:
-        print("Error")
+    temp_lanes[selected_lane].increment_num_lanes()
+    print("Num lanes:" + str(temp_lanes[selected_lane].get_num_lanes()))
+
     return Response(status=204)
 
 # Remove lane
 @app.route("/remove-lane")
 def remove_lane():
-    if selected_lane == "north":
-        temp_north_lane.decrement_num_lanes()
-        print("Num lanes:" + str(temp_north_lane.get_num_lanes()))
-    elif selected_lane == "east":
-        temp_east_lane.decrement_num_lanes()
-        print("Num lanes:" + str(temp_east_lane.get_num_lanes()))
-    elif selected_lane == "south":
-        temp_south_lane.decrement_num_lanes()
-        print("Num lanes:" + str(temp_south_lane.get_num_lanes()))
-    elif selected_lane == "west":
-        temp_west_lane.decrement_num_lanes()
-        print("Num lanes:" + str(temp_west_lane.get_num_lanes()))
-    else:
-        print("Error")
+    temp_lanes[selected_lane].decrement_num_lanes()
+    print("Num lanes:" + str(temp_lanes[selected_lane].get_num_lanes()))
     return Response(status=204)
 
 # Bus lane toggle
 @app.route("/bus-toggle")
 def bus_toggle():
-    if selected_lane == "north":
-        temp_north_lane.toggle_bus_lane()
-        print("Has bus lane:" + str(temp_north_lane.has_bus_lane()))
-    elif selected_lane == "east":
-        temp_east_lane.toggle_bus_lane()
-        print("Has bus lane:" + str(temp_east_lane.has_bus_lane()))
-    elif selected_lane == "south":
-        temp_south_lane.toggle_bus_lane()
-        print("Has bus lane:" + str(temp_south_lane.has_bus_lane()))
-    elif selected_lane == "west":
-        temp_west_lane.toggle_bus_lane()
-        print("Has bus lane:" + str(temp_west_lane.has_bus_lane()))
-    else:
-        print("Error")
+    temp_lanes[selected_lane].toggle_bus_lane()
+    print("Has bus lane:" + str(temp_lanes[selected_lane].has_bus_lane()))
     return Response(status=204)
 
 # Left turn lane
 @app.route("/left-toggle")
 def left_toggle():
-    if selected_lane == "north":
-        temp_north_lane.toggle_left_lane()
-        print("Has left lane:" + str(temp_north_lane.has_left_lane()))
-    elif selected_lane == "east":
-        temp_east_lane.toggle_left_lane()
-        print("Has left lane:" + str(temp_east_lane.has_left_lane()))
-    elif selected_lane == "south":
-        temp_south_lane.toggle_left_lane()
-        print("Has left lane:" + str(temp_south_lane.has_left_lane()))
-    elif selected_lane == "west":
-        temp_west_lane.toggle_left_lane()
-        print("Has left lane:" + str(temp_west_lane.has_left_lane()))
-    else:
-        print("Error")
+    temp_lanes[selected_lane].toggle_left_lane()
+    print("Has left lane:" + str(temp_lanes[selected_lane].has_left_lane()))
     return Response(status=204)
 
 # TODO set lane to bike lane
