5e4c3a3e97f6c6fff9c2cea1e520184a3d381c2d
Nicholas-Homann <Nicholas.Homann@warwick.ac.uk>
2025-02-26 23:22:22 +0000

    fix comments

--- build/Equations.py
+++ build/Equations.py
@@ -30,7 +30,7 @@ def get_efficiency_score(vph_rates, setup):
 
 
 
-# TODO: ask to bound it by 1 and 4 not 0 and 4
+
 def get_green_proportion(setup, direction):
     total_priority = sum([x.priority_factor for x in setup.get_all_roads()])
     this_priority = setup.get_road(direction).priority_factor
@@ -48,8 +48,6 @@ def max_queue(vph_rates, setup, direction):
                          * (proportion_green * (CYCLE_LENGTH / 60)))
     return max(0, arriving_per_cycle - leaving_per_cycle, 3/4 * arriving_per_cycle) # max of 0 and cars accumulated after one full cycle
        # NOTE: it is possible to accumulate a larger queue (=3/4 cars) before cars leave the junction
-    # TODO: Merge with left turn structure?
-    # TODO: implement environmental/pedestrian factors!!!
 
 
 # Note: max_wait is heavily affected by max_queue
@@ -61,17 +59,14 @@ def max_wait(vph_rates, setup, direction):
     return ((max_queue(vph_rates, setup, direction) / leaving_per_cycle) # Number of Cycles to remove cars
             * CYCLE_LENGTH) # length a cycle takes
     # NOTE: This has a rounding error as you can't complete, say 0.3 cycles, can round up maybe
-    # TODO: Merge with left turn structure?
-    # TODO: implement for various different configurables.
+
 
 
 def average_wait(vph_rates, setup, direction):
-    # TODO: complete base implementation
     arriving_per_cycle = ((vph_rates[direction] / setup.get_road(direction).total_standard_lanes)
                           * (CYCLE_LENGTH / 60))
     return max_wait(vph_rates, setup, direction) / arriving_per_cycle # the total time to clear the vehicles divided by the total number of vehicles arriving
-    # TODO: Merge with left turn structure?
-    # TODO: implement for various different configurables.
+
 
 
 # The average_wait time for any car at the junction
@@ -84,7 +79,8 @@ def mean_statistic(vph_rates, setup):
     return mean
 
 # the variance of the max_queue's weighted by the proportion of cars from that direction
-# used to compare ..... TODO: (COMPLETE DESCRIPTION)
+# used to compare the relative ease of moving through the junctions from different directions,
+# taking into account how busy that direction is
 def fairness_statistic(vph_rates, setup):
     total_vph = sum(vph_rates)
     values = [(vph_rates[i] / total_vph) * max_queue(vph_rates, setup, i) for i in range(0, 4)]
