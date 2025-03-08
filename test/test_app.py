174ec0a2a901142f6c03f26c1f2be4111f58cef7
tashluthra <155002572+tashluthra@users.noreply.github.com>
2025-03-08 11:18:08 +0000

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -3,7 +3,7 @@ import logging
 from flask import json
 from build.app import app  # Import the Flask app
 
-#set up logging (Logs results into `test_logs.log`)
+# Set up logging (Logs results into `test_logs.log`)
 logging.basicConfig(filename="test_logs.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 @pytest.fixture
@@ -13,38 +13,38 @@ def client():
     with app.test_client() as client:
         yield client
 
-#tests home page route
+# Tests home page route
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200 
 
-#tests edit lane selction routes 
+# Tests edit lane selction routes 
 @pytest.mark.parametrize("endpoint, expected_status", [ 
-    ("/edit-northbound", 204),
-    ("/edit-eastbound", 204),
-    ("/edit-southbound", 204),
-    ("/edit-westbound", 204)
+    ("/edit-northbound", 200),
+    ("/edit-eastbound", 200),
+    ("/edit-southbound", 200),
+    ("/edit-westbound", 200)
 ])
 def test_edit_lane(client, endpoint, expected_status):
     response = client.get(endpoint)
-    assert response.status_code == expected_status  # Should return 204 (No Content)
+    assert response.status_code == expected_status  
 
-#tests adding a lane 
+# Tests adding a lane 
 def test_add_lane(client):
     response = client.get("/add-lane")
-    assert response.status_code == 204 
+    assert response.status_code == 200 
 
-#tests removing a lane 
+# Tests removing a lane 
 def test_remove_lane(client):
     response = client.get("/remove-lane")
-    assert response.status_code == 204 
+    assert response.status_code == 200
 
-#tests toggling a bus lane 
+# Tests toggling a bus lane 
 def test_bus_toggle(client):
     response = client.get("/bus-toggle")
-    assert response.status_code == 204  
+    assert response.status_code == 200
 
-#tests toggling left lane turn 
+# Tests toggling left lane turn 
 def test_left_toggle(client):
     response = client.get("/left-toggle")
-    assert response.status_code == 204  
+    assert response.status_code == 200
